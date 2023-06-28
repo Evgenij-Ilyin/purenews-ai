28447c9f5fca60ad08367106b39de8175d325d13
Evgenij-Ilyin <e.ilyin.work@gmail.com>
2023-06-28 19:11:29 +0200

    node modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NewsModule } from './news/news.module';
-import { NewsApiService } from './news-api/news-api.service';
-import { OpenAiService } from './open-ai/open-ai.service';
+import { ConfigModule } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [NewsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    NewsModule,
+    
+  ],
   controllers: [AppController],
-  providers: [AppService, NewsApiService, OpenAiService],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/news/news.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { NewsService } from './news.service';
+import { NewsController } from './news.controller';
+
+
+@Module({
+  providers: [
+    NewsService,
+  ],
+  controllers: [NewsController]
+})
+export class NewsModule {}
